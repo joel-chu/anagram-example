321d19270542b9ca16639f5aa1ba0add857035f7
joeljiezhu <joelckchu@outlook.com>
2021-03-29 14:07:51 +0800

    The recursion problem fixed

--- python/mylib.py
+++ python/mylib.py
@@ -11,9 +11,22 @@ configFile = WORDS_DIR.joinpath('config.json')
 jsonObj = open(str(configFile))
 jsonData = json.load(jsonObj)
 # Get the number of maximum recursion calls
-maxReCall = sys.getrecursionlimit()
+# we only use 90% of what its allow on the safe side
+# in fact the number is not accurate, system report 1000 but the
+# recursion stop at 997
+recursionLimit = sys.getrecursionlimit() * 0.9
 
 # Functions
+
+def getWords(dir, name):
+    """
+    import the words file and turn into usable format (array)
+    """
+    filePath = WORDS_DIR.joinpath(jsonData['DOT'].join([str(name), jsonData['FILE_EXT']]))
+    fileObj = open(filePath)
+    fileContent = fileObj.read()
+
+    return fileContent.strip().split(' ')
 
 def getMaxTryNum(n, total = 0):
     """
@@ -47,6 +60,7 @@ def scrambleWords(str):
     scramble the characters
     although in python string is also array but it does not
     allow assignment, therefore we still need to turn it into a list
+    using comprehension
     """
     arr = [ch for ch in str]
     newArr = fisherYates(arr)
@@ -54,54 +68,53 @@ def scrambleWords(str):
     return ''.join(newArr)
 
 
-def getPossibleWord(str, triedWords):
+def getPossibleWord(str, triedWords, maxAllowTry, i = 0):
     """
     get a possible word that we haven't tried before
-    @BUG if this recursion run over 1000 times, it will throw error
-    becuase it reaches the system maximum
+    @BUG if this recursion run over 1000 (997 in fact) times,
+    it will throw RecursionError
+    because it reaches the system maximum
     """
+    # if this interaction has reach the max allow number
+    # we just terminate it and try in the next loop
+    if (i >= maxAllowTry):
+        return False
+    i += 1
+    # continue with guessing a word
     possibleWord = scrambleWords(str)
-    tempList = [x for x in triedWords if possibleWord in x]
-    print(f"triedWords array length: {len(triedWords)}")
-    if (len(tempList) > 0):
-        return getPossibleWord(str, triedWords)
+    # if its already tried then run itself again
+    if (possibleWord in triedWords):
+        return getPossibleWord(str, triedWords, maxAllowTry, i)
 
     return possibleWord
-
-def getPossibleWordInner(str, triedWords):
-    """
-    We need to split the original getPossibleWord method
-    into two parts to continue how many recursive loop its running
-    """
-
-
-def getWords(dir, name):
-    """
-    import the words file and turn into a usable format (array)
-    """
-    filePath = WORDS_DIR.joinpath(jsonData['DOT'].join([str(name), jsonData['FILE_EXT']]))
-    fileObj = open(filePath)
-    fileContent = fileObj.read()
-
-    return fileContent.strip().split(' ')
-
 
 def getAnagram(str, words):
     """
     find the anagram from the input str
+    V.2 with small changes to get around the recursion limit
     """
+    # first we check if this word is in the dict first
+    # otherwise there is no point of running the follow code
+    exist = str in words
+    if (not exist):
+        return exist
+    # filter out the provided word
     dict = [w for w in words if w != str]
-    maxTried = math.pow(2, len(str))
+    maxTried = getMaxTryNum(len(str))
     tried = 0
     possibleWords = []
-
+    # V.2 we move the while loop into the getPossibleWord
+    # because we need to check the maximum allow loop in each call
     while tried <= maxTried:
-        print(f"Tried number: {tried}")
-        word = getPossibleWord(str, possibleWords)
-        if (len([w for w in dict if w == word]) > 0):
+        # print(f"Tried number: {tried}")
+        # V.2 we add maxTry parameter
+        word = getPossibleWord(str, possibleWords, recursionLimit)
+        if (word in dict):
             return word
-
-        possibleWords.append(word)
-        ++tried
+        # if the word is False that just terminate because it reaches the maxTry
+        # we don't count that as one try
+        if (word):
+            possibleWords.append(word)
+            ++tried
 
     return False
