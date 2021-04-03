e28f84972f6abc85fa771872e7c9eccc06dcc8e9
Joel Chu <joelckchu@gmail.com>
2021-04-04 04:47:17 +0800

    restore the callback example

--- javalang/LambdaExample.java
+++ javalang/LambdaExample.java
@@ -6,32 +6,28 @@ public class LambdaExample {
   public static void main(String[] args) {
     String str = args[0];
     int count = 0;
-    // init the pubsub
-    PubSub ps = new PubSub<String>();
-
-    ps.sub((value) -> {
+    // the actual Thread
+    Thread t = new Thread(() -> someFuncToRun(str, count, (value) -> {
       System.out.println("Get result from PubSub callback");
       System.out.println(value);
 
-      return "done"; // just to match the return type
-    });
-    // the actual Thread
-    Thread t = new Thread(() -> someFuncToRun(str, count, ps));
+      return value;
+    }));
     // start running the thread
     t.start();
   }
 
-  public static String someFuncToRun(String str, int ctn, PubSub<String> ps) {
+  public static String someFuncToRun(String str, int ctn, Callback<String> p) {
     ++ctn;
     if (ctn == 10) {
-      return ps.pub("end here with result from callback");
+      return p.cb("end here with result from callback");
     }
     System.out.println(ctn + " times");
-    return someFuncToRun(str + " +1", ctn, ps);
+    return someFuncToRun(str + " +1", ctn, p);
   }
 }
 
-/* it just ridiculos 
+/* it just ridiculos
 class PubSub<T> {
 
   private Callback<T> cb;
