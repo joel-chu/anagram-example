a7833d1e20f690f8513164946793469050c3a984
Joel Chu <joelckchu@gmail.com>
2021-04-11 20:35:36 +0800

    add db.py

--- javalangV2/PubSub.java
+++ javalangV2/PubSub.java
@@ -1,33 +1,31 @@
 
 package example.anagram;
 
+import example.anagram.*;
 import java.util.*;
 // can not use generic here
+
 interface Command {
-    public void runCommand(String value);
+    public void run(String value);
 }
+// can we do PubSub<T> instead and pass it on to the Callback<T>?
+public class PubSub {
+  // when we try to init this Map store in the constructor
+  // we got an nullPointerException which means it didn't created it
+  private Map<String, Command> store = new HashMap<String, Command>();
 
-/**
-To use this stupid piece of crap
-You need to do it like this
-
-ps.sub(name, new Command() {
-  public void runCommand(String value) {
-    // this is where you put your code
-  };
-});
-
-**/
-
-class PubSub {
-
-  private Map<String,Command> store = new HashMap<String, Command>();
-
-  public void sub(String name, Command c) {
-    store.put(name, c);
+  // we init the Command inside the sub call
+  // instead we pass the Lambda as param
+  public void sub(String name, Callback<String> cbInt) {
+    store.put(name, new Command() {
+      public void run(String value) {
+        // this is wrong ...
+        cbInt.cb(value);
+      }
+    });
   }
 
   public void pub(String name, String value) {
-    store.get(name).runCommand(value);
+    store.get(name).run(value);
   }
 }
---
+++ python/db.py
@@ -0,0 +1,6 @@
+# create a small program to generate the sqlite database
+# this will get re-use by others therefore it must be READ ONLY
+# We are going to create a very simple table stucture with 3 fields
+# word (the original word)
+# characters (take the word apart then sort the characters in desc order)
+# len (the length of the word)
