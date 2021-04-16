a7a400b63837973f538df43a59174b5410826985
Joel Chu <joelckchu@gmail.com>
2021-04-16 13:59:42 +0800

    just map out some new functions for the sqlite operation

--- python/db.py
+++ python/db.py
@@ -4,3 +4,20 @@
 # word (the original word)
 # characters (take the word apart then sort the characters in desc order)
 # len (the length of the word)
+import sqlite3
+con = sqlite3.connect('../share/anagram.db')
+
+cur = con.cursor()
+
+# execute many query at once 
+
+purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+            ]
+cur.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
+
+# end loop here then commit
+
+cur.commit()
+con.close()
