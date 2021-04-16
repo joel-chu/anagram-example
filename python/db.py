c5631fa726e4ae8a56fc252bdf654f9ae6b66dbc
joeljiezhu <joelckchu@outlook.com>
2021-04-16 21:06:28 +0800

    commit the anagram db as well

--- python/db.py
+++ python/db.py
@@ -4,12 +4,52 @@
 # word (the original word)
 # characters (take the word apart then sort the characters in desc order)
 # len (the length of the word)
+
+from mylib import WORDS_DIR, jsonData, getWords
+# prepare the input data
+max = jsonData['MAX_CHAR']
+min = jsonData['MIN_CHAR']
+
+print(f"min {min} max {max}")
+
+allFiles = range(min, max + 1)
+
+print(f"{allFiles}")
+
+# place holder
+# wordsArr = []
+# @NOTE if we only use `range`  it throws a `TypeError: cannot unpack non-iterable int object`
+"""
+
+for idx, num in enumerate(allFiles):
+    print(f"i {idx} - num {num}")
+    words = getWords(WORDS_DIR, num)
+"""
+
+
+# print(wordsArr)
+
+
 import sqlite3
 con = sqlite3.connect('../share/anagram.db')
-
 cur = con.cursor()
+# only the minimum setup just use the rowid if required
+create_table_sql = "CREATE TABLE IF NOT EXISTS anagram (word TEXT, charseq TEXT)"
 
-# execute many query at once 
+cur.execute(create_table_sql)
+
+con.commit()
+con.close()
+
+
+"""
+
+
+
+
+
+
+# execute many query at once
 
 purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
              ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
@@ -19,5 +59,6 @@ cur.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
 
 # end loop here then commit
 
-cur.commit()
-con.close()
+
+
+"""
